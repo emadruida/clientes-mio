c2fd5b89b33f73c792c929f98ee0a4787c60d572
Ernesto de Miguel <ernestodemiguel@4set.es>
2023-01-19 13:54:33 +0100

    Usar el popup para confirmar el borrado

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfirmBoxEvokeService } from '@costlydeveloper/ngx-awesome-popup';
 import { Cliente } from '../cliente';
 import { ClienteService } from '../cliente.service';
 
@@ -10,7 +11,10 @@ import { ClienteService } from '../cliente.service';
 export class ClientesComponent implements OnInit {
   clientes: Cliente[] = [];
 
-  constructor(private clienteService: ClienteService) {}
+  constructor(
+    private clienteService: ClienteService,
+    private confirmBoxEvokeService: ConfirmBoxEvokeService
+  ) {}
 
   ngOnInit(): void {
     this.clienteService
@@ -19,10 +23,26 @@ export class ClientesComponent implements OnInit {
   }
 
   borrar(id: number): void {
-    this.clienteService
-      .deleteCliente(id)
-      .subscribe(
-        () => (this.clientes = this.clientes.filter((c) => id !== c.id))
-      );
+    this.confirmBoxEvokeService
+      .danger(
+        `Confirmar borrado de cliente ${id}`,
+        `¿Seguro que quieres borrar a '${this.describeCliente(id)}'?`,
+        'Sí',
+        'No'
+      )
+      .subscribe((resp) => {
+        if (resp.success) {
+          this.clienteService
+            .deleteCliente(id)
+            .subscribe(
+              () => (this.clientes = this.clientes.filter((c) => id !== c.id))
+            );
+        }
+      });
+  }
+
+  private describeCliente(id: number): string {
+    const cliente = this.clientes.find((c) => c.id === id)!;
+    return `${cliente.nombre} ${cliente.apellidos}`;
   }
 }
