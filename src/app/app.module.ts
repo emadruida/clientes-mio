c43a0c511bd05281eddd30ac0fb15e979326d0b8
Ernesto de Miguel <ernestodemiguel@4set.es>
2023-01-19 13:38:30 +0100

    Incluir estilos y módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { FormsModule } from '@angular/forms';
+import {
+  ConfirmBoxConfigModule,
+  NgxAwesomePopupModule,
+} from '@costlydeveloper/ngx-awesome-popup';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClienteDetailComponent } from './cliente-detail/cliente-detail.component';
@@ -18,9 +22,12 @@ import { ClientesComponent } from './clientes/clientes.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+
+    NgxAwesomePopupModule.forRoot(), // Esencial, módulo obligatorio para usar cualquier otro de ngx-awesome-popup
+    ConfirmBoxConfigModule.forRoot(), // Necesario para instanciar confirm boxes
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
